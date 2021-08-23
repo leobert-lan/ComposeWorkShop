e8c748b41a4b8aa6293052911521761f9e93e287
leobert-lan <774057695@qq.com>
2021-08-23 22:35:58 +0800

    feat:post27 icon sample 1

--- app/src/main/java/osp/leobert/android/composeworkshop/post27/P27IconSample.kt
+++ app/src/main/java/osp/leobert/android/composeworkshop/post27/P27IconSample.kt
@@ -1,23 +1,37 @@
 package osp.leobert.android.composeworkshop.post27
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.rounded.Call
 import androidx.compose.material.icons.sharp.Call
 import androidx.compose.material.icons.twotone.Call
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.imageResource
+import androidx.core.graphics.drawable.toBitmap
+import osp.leobert.android.composeworkshop.R
+import osp.leobert.android.composeworkshop.post21.CodeSample
 import osp.leobert.android.composeworkshop.ui.theme.ComposeWorkShopTheme
 
 class P27IconSample : ComponentActivity() {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,7 +42,6 @@ class P27IconSample : ComponentActivity() {
                     LazyColumn() {
                         item {
                             Row {
-
                                 Icon(
                                     Icons.Filled.Call,
                                     contentDescription = "Call"
@@ -52,13 +65,67 @@ class P27IconSample : ComponentActivity() {
                             }
                         }
                         item {
-
+                            CodeSample(
+                                code =
+                                """
+                                imageVector:
+                                Icon(
+                                    Icons.Filled.Call,
+                                    contentDescription = "Call",
+                                )
+                                """.trimIndent()
+                            )
+                            Icon(
+                                Icons.Filled.Call,
+                                contentDescription = "Call",
+                            )
                         }
                         item {
-
+                            CodeSample(
+                                code =
+                                """
+                                bitmap demo:
+                                Icon(
+                                    ImageBitmap.imageResource(id = R.drawable.baseline_fingerprint_black_24dp),
+                                    contentDescription = "Check fingerprint",
+                                )
+                                """.trimIndent()
+                            )
+                            Icon(
+                                ImageBitmap.imageResource(id = R.drawable.baseline_fingerprint_black_24dp),
+                                contentDescription = "Check fingerprint",
+                            )
                         }
                         item {
-
+                            CodeSample(
+                                code =
+                                """
+                                painter demo:
+                                val color = Color.Blue
+                                val painter = remember(key1 = color) {
+                                    ColorPainter(color)
+                                }
+                                Icon(
+                                    painter,
+                                    contentDescription = "Call",
+                                )
+                                """.trimIndent()
+                            )
+                            val color = Color.Blue
+                            val painter = remember(key1 = color) {
+                                ColorPainter(color)
+                            }
+                            Icon(
+                                painter,
+                                contentDescription = "Call",
+                            )
+                        }
+                        item {
+                            Icon(
+                                ImageBitmap.imageResource(id = R.drawable.baseline_fingerprint_black_24dp),
+                                contentDescription = "Check fingerprint",
+                                tint = Color.LightGray
+                            )
                         }
 
                     }
