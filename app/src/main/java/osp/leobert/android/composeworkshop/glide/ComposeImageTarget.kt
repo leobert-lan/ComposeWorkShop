1c764acc5056c8220956b8116e782a14c4d3459d
leobert-imac <774057695@qq.com>
2021-09-01 10:43:16 +0800

    feat: post 27 网络资源加载错误示例

--- app/src/main/java/osp/leobert/android/composeworkshop/glide/ComposeImageTarget.kt
+++ app/src/main/java/osp/leobert/android/composeworkshop/glide/ComposeImageTarget.kt
@@ -6,25 +6,19 @@ import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.lifecycle.MutableLiveData
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 
-/**
- * <p><b>Package:</b> osp.leobert.android.composeworkshop.glide </p>
- * <p><b>Project:</b> ComposeWorkShop </p>
- * <p><b>Classname:</b> ComposeImageTarget </p>
- * <p><b>Description:</b> TODO </p>
- * Created by leobert on 2021/8/31.
- */
-class ComposeImageTarget(var imgRes: MutableState<ImageBitmap>) : CustomTarget<Bitmap>() {
+class ComposeImageTarget(private var imgRes: MutableLiveData<ImageBitmap>) :
+    CustomTarget<Bitmap>() {
 
     override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-        Log.e("lmsg","onResourceReady:")
         imgRes.value = resource.asImageBitmap()
     }
 
     override fun onLoadCleared(placeholder: Drawable?) {
+//        imgRes.value.asAndroidBitmap().recycle()
+        Log.e("lmsg", "onLoadCleared")
     }
-
-
 }
--- app/src/main/java/osp/leobert/android/composeworkshop/post27/P27ImageSample.kt
+++ app/src/main/java/osp/leobert/android/composeworkshop/post27/P27ImageSample.kt
@@ -1,18 +1,23 @@
 package osp.leobert.android.composeworkshop.post27
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -20,12 +25,18 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import osp.leobert.android.composeworkshop.R
 import osp.leobert.android.composeworkshop.glide.ComposeImageTarget
 import osp.leobert.android.composeworkshop.ui.theme.ComposeWorkShopTheme
 
 class P27ImageSample : ComponentActivity() {
+    val imgLiveData by lazy {
+        MutableLiveData(ImageBitmap.imageResource(this.resources, id = R.drawable.text_demo))
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,27 +74,72 @@ class P27ImageSample : ComponentActivity() {
                         }
 
                         item {
-                            val res = ImageBitmap.imageResource(id = R.drawable.text_demo)
 
-                            val imgRes = remember { mutableStateOf(res) }
+                            Column {
 
-                            Image(
-                                imgRes.value,
-                                contentDescription = "Check fingerprint",
-                                modifier = Modifier.size(200.dp)
-                            )
+                                val res = ImageBitmap.imageResource(id = R.drawable.text_demo)
 
-                            if (imgRes.value == res)
-                                Glide.with(this@P27ImageSample)
-                                    .asBitmap()
-                                    .load("https://img1.baidu.com/it/u=1678797598,221669148&fm=26&fmt=auto&gp=0.jpg")
-                                    .placeholder(R.drawable.text_demo)
-                                    .into(ComposeImageTarget(imgRes))
+                                val imgRes = imgLiveData.observeAsState(res)
+
+                                Log.e("lmsg", "compute ${imgRes.value == res}")
+                                Image(
+                                    imgRes.value,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .background(Color.LightGray)
+                                )
+                                Spacer(modifier = Modifier.size(10.dp))
+                                Row {
+                                    Button(onClick = { loadImage1() }) {
+                                        Text(text = "加载图片1")
+                                    }
+
+                                    Spacer(modifier = Modifier.size(10.dp))
+
+                                    Button(onClick = { loadImage2() }) {
+                                        Text(text = "加载图片2")
+                                    }
+
+                                    Spacer(modifier = Modifier.size(10.dp))
+
+                                    Button(onClick = { loadImage3() }) {
+                                        Text(text = "加载图片3")
+                                    }
+                                }
+                            }
                         }
-
                     }
                 }
             }
         }
+    }
+
+    private fun loadImage1() {
+        Glide.with(this@P27ImageSample)
+            .asBitmap()
+            .load("https://img1.baidu.com/it/u=1678797598,221669148&fm=26&fmt=auto&gp=0.jpg")
+            .into(ComposeImageTarget(imgLiveData))
+    }
+
+    private fun loadImage2() {
+        Glide.with(this@P27ImageSample)
+            .asBitmap()
+            .load("https://img0.baidu.com/it/u=2244828323,710305807&fm=11&fmt=auto&gp=0.jpg")
+            .into(ComposeImageTarget(imgLiveData))
+    }
+
+    private fun loadImage3() {
+        Glide.with(this@P27ImageSample)
+            .asBitmap()
+            .load("https://img1.baidu.com/it/u=2882162007,1208714680&fm=11&fmt=auto&gp=0.jpg")
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(ComposeImageTarget(imgLiveData))
+
+        Glide.with(this@P27ImageSample)
+            .asBitmap()
+            .load("https://img0.baidu.com/it/u=2244828323,710305807&fm=11&fmt=auto&gp=0.jpg")
+            .into(ComposeImageTarget(imgLiveData))
     }
 }
